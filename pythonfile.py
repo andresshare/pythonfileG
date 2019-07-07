c736bc3d4b926a716e61c3875a26ab69a8061f2e
andresshare <andresshare@gmail.com>
2019-07-06 21:27:58 -0500

    start dragon

--- pythonfile.py
+++ pythonfile.py
@@ -1,32 +1,44 @@
 import random
-intentos_realizados = 0
-print("Como te llamas?")
-miNombre = input()
+import time
 
-numero = random.randint(1, 20)
-print('OK, '  + miNombre +  " estoy pensando en un numero entre 1 y 20 ")
+def mostrarIntroduccion():
+    print('Estás en una tierra llena de dragones. Frente a tí')
+    print('hay dos cuevas. En una deellas, el dragón es generoso y')
+    print('amigable y compartirá su tesoro contigo. El otro dragón')
+    print('es codicioso y está hambriento, y te devorará inmediatamente.')
+    print()
 
-while intentos_realizados < 6:
-    print("intenta Adivinar")
-    estimacion = input()
-    estimacion = int(estimacion)
+def elegirCueva():
+    cueva = ""
+    while cueva != "1" and cueva != "2":
+        print("puerta que quieres entrar?(1 o 2)")
+        cueva = input()
+    return cueva
 
-    intentos_realizados = intentos_realizados + 1
 
-    if estimacion < numero:
-         print("Tu estimacion es muy baja")
+def explorarcueva(cuevaElegida):
+    print("Te acercas a la cueva....")
+    time.sleep(2)
+    print("Es oscura y espeluznante")
+    time.sleep(2)
+    print("Un gran dragon  aparece subitamente frente a ti! Abre su Boca")
+    print()
+    time.sleep(2)
 
-    if estimacion > numero:
-        print("Tu estimacion es muy alta")
+    cuevaAmigable = random.randint(1,2)
 
-    if  estimacion == numero:
-        break
-if estimacion == numero:
-    intentos_realizados = str(intentos_realizados)
-    print("ok" + miNombre +" Adivinaste en " + intentos_realizados + "intentos")
+    if cuevaElegida ==str(cuevaAmigable):
+        print("Te regala su tesoro")
+    else:
+        print("Te come de un solo bocado")
 
-if estimacion != numero:
-    numero = str(numero)
-    print("Error, No era el numero que pensaba " +  numero)
+
+jugardeNuevo = 'si'
+while jugardeNuevo == 'si' or jugardeNuevo == "s":
+    mostrarIntroduccion()
+    numerodecueva = elegirCueva()
+    explorarcueva(numerodecueva)
+    print("Quiere jugar de nuevo(si,no)")
+    jugardeNuevo = input()
 
 
